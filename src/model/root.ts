1d57227cffa88bde22a61176640e75d15dd901ae
Sébastien <sgregoire@users.noreply.github.com>
2023-07-25 21:30:35 +0200

    Add occurence to events

--- src/model/root.ts
+++ src/model/root.ts
@@ -1,16 +1,28 @@
 
 export type Root = {
   eventTypes: ReadonlyArray<EventType>;
+  exceptions: ReadonlyArray<Timeframe>;
 }
 
 export type EventType = {
   name: string;
+  tags: ReadonlyArray<string>;
   color?: number;
   timeframe: Timeframe;
-  exceptions: ReadonlyArray<Timeframe>;
+  exceptions: ReadonlyArray<Timeframe>
+  occurences: ReadonlyArray<Occurence>
 }
 
 export type Timeframe = {
   from: date;
   to: date;
+}
+
+// TODO @sgregoire: change to bits
+type Day = "Mo" | "Tu" | "We" | "Th" | "Fr" | "Sa" | "Su";
+
+export type Occurence = {
+  day: Day;
+  from: time;
+  to: time;
 }
