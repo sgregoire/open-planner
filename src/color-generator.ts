d0b8f9ba6c7121487c01dec4f47ca3ab4e9a1f46
Sebastien GREGOIRE <sebastien.gregoire@amadeus.com>
2023-08-06 13:48:26 +0200

    Fix color generator float operations

--- src/color-generator.ts
+++ src/color-generator.ts
@@ -2,7 +2,7 @@
 const maxAmount = 0xFFFFFF;
 
 export function generateColors(nbColor: number) {
-  const steps = maxAmount / nbColor;
+  const steps = Math.trunc(maxAmount / (nbColor + 1));
   let color = 0;
   const res = [];
   for(let i = 0; i < nbColor; i++) {
