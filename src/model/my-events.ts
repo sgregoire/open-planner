613805e844d54bfd0a52dc4513f6ffcfa3d8bb7f
Sebastien GREGOIRE <sgregoire91@gmail.com>
2023-09-15 23:12:10 +0200

    Add event generator

--- src/model/my-events.ts
+++ src/model/my-events.ts
@@ -1,5 +1,5 @@
 import moment from 'moment'
-import type { Root } from './root'
+import type { Day, EventType, Hour, Occurence, Root } from './root'
 
 export const root: Root = {
     timeframe: {
@@ -129,4 +129,59 @@ export const root: Root = {
             to: moment("2023-10-01").toDate(),
         }
     ]
+}
+
+function indexToDay(idx: number): Day {
+    switch (idx) {
+        case 1: return "Mo";
+        case 2: return "Tu";
+        case 3: return "We";
+        case 4: return "Th";
+        case 5: return "Fr";
+        case 6: return "Sa";
+        default: return "Su";
+    };
+}
+
+export function weekEventGenerator(number: number) {
+    const eventTypes: EventType[] = [];
+    for(let i = 0; i < number; i++) {
+        const nbOccurences = Math.floor(Math.random() * 5);
+        const occurences: Occurence[] = [];
+        for(let j = 0; j < nbOccurences; j++) {
+            const start = Math.floor(Math.random() * 12) + 8
+            const duration = Math.floor(Math.random() * 2) + 1;
+            occurences.push({
+                day: indexToDay(Math.floor(Math.random() * 7)),
+                from: {
+                    hour: start as Hour,
+                    minute: 0,
+                },
+                to: {
+                    hour: start + duration as Hour,
+                    minute: 0,
+                }
+            });
+        }
+        eventTypes.push({
+            name: `event-${i}`,
+            exceptions: [],
+            tags: new Set(),
+            timeframe: {
+                from: moment("2023-09-01").toDate(),
+                to: moment("2024-06-01").toDate(),
+            },
+            occurences,
+        })
+
+    }
+    const root: Root = {
+        timeframe: {
+            from: moment("2023-09-01").toDate(),
+            to: moment("2024-06-01").toDate(),
+        },
+        exceptions: [],
+        eventTypes,
+    }
+    return root;
 }
