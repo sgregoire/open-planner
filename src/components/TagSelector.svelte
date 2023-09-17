667e0cebd9fa87e3121466139ce00f6781a4a521
Sebastien GREGOIRE <sgregoire91@gmail.com>
2023-09-17 11:22:35 +0200

    format

--- src/components/TagSelector.svelte
+++ src/components/TagSelector.svelte
@@ -4,9 +4,9 @@
   export let eventTypes: EventType[];
   export let selected: string[];
   export let selectionCallback: (tags: string[]) => void;
-  
+
   const tags = new Set(eventTypes.flatMap((it) => Array.from(it.tags)));
-  
+
   function tagSelected(tag: string, currentlyChecked: boolean) {
     if (currentlyChecked) {
       selectionCallback(selected.filter((it) => it != tag));
