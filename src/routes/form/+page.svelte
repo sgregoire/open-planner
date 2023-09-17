0cccd61b5f7af32dc271ac3acfcc0c1fe8a83a77
Sebastien GREGOIRE <sgregoire91@gmail.com>
2023-09-17 10:57:40 +0200

    fix form mapping

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,72 +1,93 @@
 <script lang="ts">
-  import { Day, type EventType } from '../../model';
+    import { writable } from 'svelte/store';
+  import { Day, type EventType, type Occurence } from '../../model';
 
-  const eventInfo: EventType = {
+  type EditableTimeframe = {
+    from: string,
+    to: string
+  }
+
+  function dateToString(date: Date) {
+    return [
+      date.getFullYear(), 
+      (date.getMonth() + 1).toString().padStart(2, '0'), 
+      date.getDate().toString().padStart(2, '0')
+    ].join('-');
+  }
+
+  const event = writable({
     name: '',
-    exceptions: [],
-    occurences: [],
-    tags: [],
+    color: '#FFFFFF',
     timeframe: {
-      from: new Date(),
-      to: new Date(),
-    },
-  };
+      from: dateToString(new Date()),
+      to: dateToString(new Date()),
+    } as EditableTimeframe,
+    exceptions: [] as EditableTimeframe[],
+    occurences: [] as Occurence[],
+    tags: [] as string[],
+  })
 
   function addTag() {
-    eventInfo.tags.push('');
+    $event.tags = [...$event.tags, ''];
   }
 
   function deleteTag(index: number) {
-    eventInfo.tags.splice(index, 1);
+    $event.tags = $event.tags.toSpliced(index, 1);
   }
 
   function addException() {
-    eventInfo.exceptions.push({
-      from: new Date(),
-      to: new Date(),
-    });
+    $event.exceptions = [
+      ...$event.exceptions,
+      {
+        from: dateToString(new Date()),
+        to: dateToString(new Date()),
+      }
+    ];
   }
 
   function deleteException(index: number) {
-    eventInfo.exceptions.splice(index, 1);
+    $event.exceptions = $event.exceptions.toSpliced(index, 1);
   }
 
   function addOccurence() {
-    eventInfo.occurences.push({
-      day: Day.Mo,
-      from: {
-        hour: 10,
-        minute: 30,
-      },
-      to: {
-        hour: 11,
-        minute: 30,
-      },
-    });
+    $event.occurences = [
+      ...$event.occurences,
+      {
+        day: Day.Mo,
+        from: {
+          hour: 10,
+          minute: 30,
+        },
+        to: {
+          hour: 11,
+          minute: 30,
+        },
+      }
+    ];
   }
 
   function deleteOccurence(index: number) {
-    eventInfo.occurences.splice(index, 1);
+    $event.occurences = $event.occurences.toSpliced(index, 1);
   }
 </script>
 
 <form>
   <label for="event-name">Event name:</label>
-  <input id="event-name" type="text" bind:value={eventInfo.name} />
-
+  <input id="event-name" type="text" bind:value={$event.name} />
+<!-- 
   <label for="event-color">Color:</label>
-  <input id="event-color" type="color" bind:value={eventInfo.color} />
+  <input id="event-color" type="color" bind:value={event.color} /> -->
 
   <label for="event-from">From:</label>
-  <input id="event-from" type="date" bind:value={eventInfo.timeframe.from} />
+  <input id="event-from" type="date" bind:value={$event.timeframe.from} />
 
   <label for="event-to">To:</label>
-  <input id="event-to" type="date" bind:value={eventInfo.timeframe.to} />
+  <input id="event-to" type="date" bind:value={$event.timeframe.to} />
 
   <fieldset>
     <legend>Occurences</legend>
     <input type="button" value="Add" on:click={addOccurence} />
-    {#each eventInfo.occurences as occurence, i}
+    {#each $event.occurences as occurence, i}
       <input type="button" value="Remove" on:click={() => deleteOccurence(i)} />
 
       <label for={`event-occurence-${i}-day`}>Day</label>
@@ -83,7 +104,7 @@
   <fieldset>
     <legend>Exceptions</legend>
     <input type="button" value="Add" on:click={addException} />
-    {#each eventInfo.exceptions as exception, i}
+    {#each $event.exceptions as exception, i}
       <input type="button" value="Remove" on:click={() => deleteException(i)} />
 
       <label for={`event-exception-${i}-from`}>From</label>
@@ -97,11 +118,11 @@
   <fieldset>
     <legend>Tags</legend>
     <input type="button" value="Add" on:click={addTag} />
-    {#each eventInfo.tags as tag, i}
+    {#each $event.tags as tag, i}
       <input type="button" value="Remove" on:click={() => deleteTag(i)} />
 
       <label for={`event-tags-${i}`}>Tag {i + 1}</label>
-      <input id={`event-tags-${i}`} type="date" bind:value={tag} />
+      <input id={`event-tags-${i}`} bind:value={tag} />
     {/each}
   </fieldset>
 </form>
