42fa7f95b617fc671949650bc1cb5cba8a457111
Sebastien GREGOIRE <sebastien.gregoire@amadeus.com>
2023-09-17 16:15:36 +0200

    Add context in navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import '../app.css';
   import ExportEvents from '../components/ExportEvents.svelte';
   import ImportEvents from '../components/ImportEvents.svelte';
+  import CalendarWeekend from 'svelte-material-icons/CalendarWeekend.svelte';
+  import CalendarMonth from 'svelte-material-icons/CalendarMonth.svelte';
+  import Pencil from 'svelte-material-icons/Pencil.svelte';
+  import { hasContext } from 'svelte';
 </script>
 
 <div class="w-full bg-primary mb-3 px-6 py-2 flex flex-auto">
@@ -11,6 +15,12 @@
     </a>
   </div>
   <div class="flex grow justify-end gap-x-4">
+    {#if hasContext('root')}
+      <a href="/weekly" class="btn btn-ghost"><CalendarWeekend />weekly</a>
+      <a href="/yearly" class="btn btn-ghost"><CalendarMonth />Yearly</a>
+    {:else}
+      <a href="/form" class="btn btn-ghost"><Pencil />Create</a>
+    {/if}
     <ImportEvents />
     <ExportEvents />
   </div>
