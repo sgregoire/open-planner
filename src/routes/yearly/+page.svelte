5d54827a105a3ee0f112a54029a322fa5a983878
Sebastien GREGOIRE <sgregoire91@gmail.com>
2023-09-23 08:55:17 +0200

    Always show events yearly

--- src/routes/yearly/+page.svelte
+++ src/routes/yearly/+page.svelte
@@ -6,7 +6,6 @@
   import { momentDateToDay } from '../../model/momentUtils';
   import type { Root } from '../../model';
   import type { Writable } from 'svelte/store';
-  import NothingToShow from '../../components/NothingToShow.svelte';
 
   const rootContext = getContext<Writable<Root>>('root');
   const monthNames = [
@@ -23,6 +22,8 @@
     'Novembre',
     'Décembre',
   ];
+
+  const dayNames = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
 
   function computeDuration(root: Root) {
     return moment.duration(moment(root.timeframe.to).endOf('month').diff(moment(root.timeframe.from).startOf('month')));
@@ -42,33 +43,29 @@
   }
 </script>
 
-{#if $rootContext}
-  <div class="flex flex-1">
-    {#each months as month}
-      <div class="flex flex-col flex-1">
-        <div class="text-3xl font-bold text-center">
-          {monthNames[month[0].month()]}
-        </div>
-        {#each month as day}
-          <div class="p-1 flex flex-row border">
-            <div class="mr-1">{day.date()}</div>
-            <div class="mr-2">{momentDateToDay(day.day())}</div>
-            <div class="flex flex-1">
-              {#if matchingEvents(day, $rootContext).length > 0}
-                {#each matchingEvents(day, $rootContext) as event}
-                  <div class="h-full w-full px-2" style="background-color: {colorToRgb(event.color)}">
-                    {event.name}
-                  </div>
-                {/each}
-              {:else}
-                <div class="py-1" />
-              {/if}
-            </div>
-          </div>
-        {/each}
+<div class="flex flex-1">
+  {#each months as month}
+    <div class="flex flex-col flex-1">
+      <div class="text-3xl font-bold text-center">
+        {monthNames[month[0].month()]}
       </div>
-    {/each}
-  </div>
-{:else}
-  <NothingToShow />
-{/if}
+      {#each month as day}
+        <div class="p-1 flex flex-row border">
+          <div class="mr-1">{day.date().toString().padStart(2, '0')}</div>
+          <div class="mr-2 w-24">{dayNames[momentDateToDay(day.day())]}</div>
+          <div class="flex flex-1">
+            {#if matchingEvents(day, $rootContext).length > 0}
+              {#each matchingEvents(day, $rootContext) as event}
+                <div class="h-full w-full px-2" style="background-color: {colorToRgb(event.color)}">
+                  {event.name}
+                </div>
+              {/each}
+            {:else}
+              <div class="py-1" />
+            {/if}
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
