2d3171c8eb82d1ddf5c89c89e651f2fed8ede82b
Sebastien GREGOIRE <sebastien.gregoire@amadeus.com>
2023-11-17 23:33:38 +0100

    add loader

---
+++ src/components/Loading.svelte
@@ -0,0 +1,3 @@
+<div class="w-full">
+  <div class="rounded-md h-16 w-16 border-4 border-t-4 border-slate-700 animate-spin mx-auto" />
+</div>
--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -3,26 +3,34 @@
   import { dateToString, editableRootToRoot, rootToEdiable } from '../../model/form/mappers';
   import RootForm from '../../components/form/RootForm.svelte';
   import type { EditableRoot } from '../../model/form/EditableRoot';
-  import { getContext } from 'svelte';
+  import { getContext, onMount } from 'svelte';
   import type { Root } from '../../model';
   import ContentSave from 'svelte-material-icons/ContentSave.svelte';
+  import Loading from '../../components/Loading.svelte';
 
   const rootContextStore: Writable<Root> = getContext('root');
 
-  const writableEditableRootStore = writable<EditableRoot>(rootToEdiable($rootContextStore));
-  rootContextStore.subscribe((root) => {
-    writableEditableRootStore.set(rootToEdiable(root));
-  });
+  const writableEditableRootStore = writable<EditableRoot>();
 
   function submit() {
     rootContextStore.set(editableRootToRoot($writableEditableRootStore));
   }
+
+  onMount(() => {
+    rootContextStore.subscribe((root) => {
+      writableEditableRootStore.set(rootToEdiable(root));
+    });
+  });
 </script>
 
-<form on:submit|preventDefault={submit}>
-  <RootForm bind:root={$writableEditableRootStore} />
+{#if $writableEditableRootStore}
+  <form on:submit|preventDefault={submit}>
+    <RootForm bind:root={$writableEditableRootStore} />
 
-  <button class="btn btn-success mt-2" on:click={submit}>
-    <ContentSave /> Save
-  </button>
-</form>
+    <button class="btn btn-success mt-2" on:click={submit}>
+      <ContentSave /> Save
+    </button>
+  </form>
+{:else}
+  <Loading />
+{/if}
